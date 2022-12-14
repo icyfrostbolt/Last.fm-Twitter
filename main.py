f85db61d82e8280d0b5d85c163f2e4eef0c6ba06
Ryan Lowe <ryclowe@gmail.com>
2022-12-14 23:04:41 +0000

    Added a top songs chart

--- main.py
+++ main.py
@@ -59,8 +59,31 @@ def tweet_image(url, message):
     else:
         print("Unable to download image")
 
+def rank_tweeter(ranked_list,text):
+    message = f"Top {text} for last 1000 listens:"
+    if len(ranked_list) > 5:
+        for a in range(5):
+            message = message + f"\n{a+1:}. {ranked_list[a][0]} - {ranked_list[a][1]} plays"
+            # call to find image album.getInfo, artist.getInfo, track.getInfo then add them to the tweet
+    else:
+        for a in range(len(ranked_list)):
+            message = message + f"\n{a+1:}. {ranked_list[a][0]} - {ranked_list[a][1]} plays"
+            # call to find image
+    twitter.create_tweet(text=message)
+
+def rank():
+   ranked_artists = sorted(play_count_artist.items(), key=lambda x: x[1], reverse=True)
+   rank_tweeter(ranked_artists,"artists")
+   ranked_albums = sorted(play_count_album.items(), key=lambda x: x[1], reverse=True)
+   rank_tweeter(ranked_albums,"albums")
+   ranked_songs = sorted(play_count_song.items(), key=lambda x: x[1], reverse=True)
+   rank_tweeter(ranked_songs,"songs")
+
 counter = 0
 page_num = 5
+play_count_artist = {}
+play_count_album = {}
+play_count_song = {}
 work = False
 while work == False:
   while page_num > 0:
@@ -73,11 +96,21 @@ while work == False:
     jprint(r.json())
     try: # This stops it from breaking because of no recenttracks key
       noerr = True
-      print(work)
       for t in r.json()['recenttracks']['track']:
           if not "@attr" in t:
-              tracks.append(
-              [[f"{t['name']} by {t['artist']['#text']}",[t['date']['#text'].split()],[t['image'][3]['#text']]]]) # the number between ['image] and ['text] represents size
+              tracks.append([[f"{t['name']} by {t['artist']['#text']}",[t['date']['#text'].split()],[t['image'][3]['#text']]]]) # the number between ['image] and ['text] represents size
+              if t['artist']['#text'] in play_count_artist:
+                  play_count_artist[t['artist']['#text']] += 1
+              else:
+                  play_count_artist[t['artist']['#text']] = 0
+              if f"{t['album']['#text']} - {t['artist']['#text']}" in play_count_album:
+                  play_count_album[f"{t['album']['#text']} - {t['artist']['#text']}"] += 1
+              else:
+                  play_count_album[f"{t['album']['#text']} - {t['artist']['#text']}"] = 0
+              if f"{t['name']} - {t['artist']['#text']}" in play_count_song:
+                  play_count_song[f"{t['name']} - {t['artist']['#text']}"] += 1
+              else:
+                  play_count_song[f"{t['name']} - {t['artist']['#text']}"] = 0
       # splits hour and minutes
       tracks[-1][-1][1][0][3] = tracks[-1][-1][1][0][3].split(":")
       for z in tracks[-1][-1][1][0][3]:
@@ -97,8 +130,8 @@ while work == False:
     if most_recent == "":
       # Requests most recent tweets from a users timeline
       tweets = twitter.get_users_tweets(id=user_id,max_results=5)
-      try: #adapt tis to find the most recent one that fits in case sharing with multipel programs
-        tweets = str(tweets[0][0])
+      try: #adapt tis to find the most recent one that fits in case sharing with multipel programs   
+        tweets = str(tweets[0][3])
         most_recent = datetime(int(tweets[-28:-24]),int(tweets[-31:-29]),int(tweets[-34:-32]),int(tweets[-43:-41]),int(tweets[-40:-38]),0,0)
       except:
         if ":" in s[-1][1][0]:
@@ -106,7 +139,7 @@ while work == False:
         date_data = s[-1][1][0]
         most_recent = datetime(int(date_data[2]), int(date_data[1]), int(date_data[0]), int(date_data[3]), int(date_data[4]), 0, 0)
         if most_recent == "":
-            date_data = s[-1][1][0]
+            date_data = s[0][1][0]
             most_recent = datetime(int(date_data[2]),int(date_data[1]),int(date_data[0]),int(date_data[3]),int(date_data[4]),0,0)
     date_data = s[-1][1][0]
     try:
@@ -123,4 +156,5 @@ while work == False:
       most_recent = current_time
       time.sleep(1)
   if noerr:
+    rank()
     work = True
